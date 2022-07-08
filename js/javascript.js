e56a9b68956d7a1ef279493ba69a17ba66371bd2
sheeshpal kotwal <sheeshpalkotwal@gmail.com>
2022-07-08 13:06:07 +0530

    final all changes

--- js/javascript.js
+++ js/javascript.js
@@ -2,12 +2,3 @@ document.documentElement.style.setProperty('--animate-duration', '2s');
 
 // All animations will take half the time to accomplish
 document.documentElement.style.setProperty('--animate-duration', '.5s');
-
-
-const element = document.querySelector('.my-element');
-element.classList.add('animate__animated', 'animate__bounceOutLeft');
-
-element.addEventListener('animationend', () => {
-  // do something
-});
-
